45a5d92cf0b0b6c3f1599d4d8fee60c294147d10
Kevin Fairise <kevin.fairise@student-cs.fr>
2022-10-18 15:06:07 +0200

    add sentry

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "./App.css";
 import { CalculatorPage } from "./pages";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://2f7ac8876d054898b4fb31c628235aaf@o4504004729438208.ingest.sentry.io/4504004733894656",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const App = () => {
   return (
